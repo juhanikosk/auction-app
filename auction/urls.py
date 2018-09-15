2125fc76ad3413c042d25e50f7c4f0250d8edd6e
Juhani Koskinen <juhanikos@gmail.com>
2018-09-15 20:39:55 +0300

    Changed folder structure

--- auction/urls.py
+++ auction/urls.py
@@ -1,4 +1,4 @@
-"""schoolproject URL Configuration
+"""auction URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -18,8 +18,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import admin
 from django.urls import path
 
-from schoolproject.views.login import CreateUserView, LoginView
-from schoolproject.views.auctions import IndexView
+from auction.views.login import CreateUserView, LoginView
+from auction.views.auctions import IndexView
 
 
 def login_wrap(view_class, template=None):
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'schoolproject.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auction.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
