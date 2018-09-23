14101f71bbc012589ab7dc55d0d8d8b22425a6c4
Juhani Koskinen <juhanikos@gmail.com>
2018-09-23 15:22:05 +0300

    Enhance validators for User information.

--- auction/models/user.py
+++ auction/models/user.py
@@ -6,7 +6,7 @@ from django.core import validators
 class AuctionUser(AbstractUser):
     first_name = models.CharField(verbose_name="First name", max_length=128, validators=[validators.MinLengthValidator(3)])
     last_name = models.CharField(verbose_name="Last name", max_length=128, validators=[validators.MinLengthValidator(3)])
-    phone = models.CharField(verbose_name="Phonenumber", max_length=28)
+    phone = models.CharField(verbose_name="Phonenumber", max_length=28, validators=[validators.RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Enter a valid phonenumber.")])
 
     def __str__(self):
         return "{} {} | {}".format(self.first_name, self.last_name, self.email)
